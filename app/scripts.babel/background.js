889e005edc8c89068e556c8251cf45ad8bad5149
Benjamin Congdon <bencongdon96@gmail.com>
2016-12-28 08:03:27 -0800

    Automatic redirect to watchLater version of video

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -4,4 +4,15 @@ chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
 
-console.log('\'Allo \'Allo! Event Page');
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    var newurl = details.url + '&list=WL'
+    if(details.url.indexOf('list=') == -1) {
+      console.log('Redirecting ' + details.url + ' to ' + newurl)
+      return { redirectUrl: newurl }
+    }
+  },
+  {urls: ['*://youtube.com/watch?*',
+          '*://www.youtube.com/watch?*']},
+  ['blocking']
+);
